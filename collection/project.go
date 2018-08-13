2f45eafe6058dc71e86973505ef9375e3e478cf0
nishikata-seiichi <seiichi_nishikata@r.recruit.co.jp>
2018-08-13 11:07:53 +0900

    Fix collection structure

--- collection/project.go
+++ collection/project.go
@@ -9,11 +9,5 @@ type Project struct {
 	ID          bson.ObjectId `json:"_id" bson:"_id"`
 	Name        string        `json:"name" bson:"name"`
 	Description string        `json:"description" bson:"description"`
-	APIKeys     []APIKey      `json:"api_keys" bson:"api_keys"`
-	Features    []Feature     `json:"features" bson:"features"`
-}
-
-// APIKey is nested structure
-type APIKey struct {
-	Key string `json:"key" bson:"key"`
+	APIKeys     []string      `json:"api_keys" bson:"api_keys"`
 }
