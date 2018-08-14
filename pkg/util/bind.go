2965f3fd64f33d13ba65b8be723bb935b3933200
nishikata-seiichi <seiichi_nishikata@r.recruit.co.jp>
2018-08-14 12:05:54 +0900

    Fix golint check point `redundant if ...; err != nil check, just return error instead.`

--- pkg/util/bind.go
+++ pkg/util/bind.go
@@ -15,11 +15,8 @@ func BindParam(r *http.Request, dst interface{}) error {
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
-	if err := decoder.Decode(dst, r.Form); err != nil {
-		return err
-	}
 
-	return nil
+	return decoder.Decode(dst, r.Form)
 }
 
 // BindJSONParam bind to request body to struct
